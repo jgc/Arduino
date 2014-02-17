48e7dbe62c2791880fae2e111ed4bd607be5f842
jgc <jgcy@yahoo.com>
2014-02-17 22:38:40 +0000

    V4

--- Attiny84_test_blink_led_1/Attiny84_test_blink_led_1.ino
+++ Attiny84_test_blink_led_1/Attiny84_test_blink_led_1.ino
@@ -1,28 +1,26 @@
 int c = 0;
 int LED = 10;
 
-static void led (bool on) {
-  digitalWrite(LED, on ? 0 : 1); // inverted logic
-}
-
 void setup() {
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
   digitalWrite(LED, 1);
-  delay(5000);
+  delay(1000);
   digitalWrite(LED, 0);
+  delay(5000);
 }
 
 void loop() {
-  c++;
   for (int x = 1; x <= c; x++){
-    led(true);
-    delay(1000);
-    led(false);
+    digitalWrite(LED, 1);
+    delay(500);
+    digitalWrite(LED, 0);
     delay(500);
   }
-  if (c == 10){
+  c++;
+  if (c == 5){
     c = 0;
   }
-  delay(3000);
+  delay(2500);
 }
+
