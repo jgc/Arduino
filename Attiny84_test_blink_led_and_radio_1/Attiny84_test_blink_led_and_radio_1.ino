af0a12896f6e3abc49ac7c342cd9725dfeaa34c8
jgc <jgcy@yahoo.com>
2014-02-18 19:09:14 +0000

    V2

--- Attiny84_test_blink_led_and_radio_1/Attiny84_test_blink_led_and_radio_1.ino
+++ Attiny84_test_blink_led_and_radio_1/Attiny84_test_blink_led_and_radio_1.ino
@@ -1,5 +1,5 @@
 #include <JeeLib.h>
-//#include <PortsSHT11.h>
+#include <PortsSHT11.h>
 #include <avr/sleep.h>
 #include <util/atomic.h>
 
@@ -8,19 +8,28 @@ int LED = 10;
 byte counter;
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(38400);
+  //Serial.print("\nSerial baud 38400");
   rf12_initialize(1, RF12_868MHZ, 100);
   pinMode(LED, OUTPUT);
   digitalWrite(LED, 1);
   delay(1000);
   digitalWrite(LED, 0);
-  delay(5000);
+  delay(2000);
 }
 
 void loop() {
+  //Serial.print("Counter = ");
+  //Serial.println(c);
   for (int x = 1; x <= c; x++){
+    counter = B1;
+    //rf12_sendNow(0, &counter, 1);
+    //rf12_sendWait(1);
     digitalWrite(LED, 1);
     delay(500);
+    counter = B0;
+    //rf12_sendNow(0, &counter, 1);
+    //rf12_sendWait(1);
     digitalWrite(LED, 0);
     delay(500);
   }
@@ -28,6 +37,6 @@ void loop() {
   if (c == 5){
     c = 0;
   }
-  delay(2500);
+  delay(1500);
 }
 
